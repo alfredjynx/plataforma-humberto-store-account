fc585c968b18fe4a4b2400c463d6dd0b587c8f42
alfredjynx <99195331+alfredjynx@users.noreply.github.com>
2024-02-23 09:09:47 -0300

    novas coisas

--- src/main/java/humberto/loja/account/AccountController.java
+++ src/main/java/humberto/loja/account/AccountController.java
@@ -1,16 +1,22 @@
 package humberto.loja.account;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@FeignClient(name = "spkio-account")
 public interface AccountController {
 
     @PostMapping("/accounts")
     public ResponseEntity<AccountOut> create(
         @RequestBody(required = true) AccountIn in);
+
+    @PostMapping("/accounts/login")
+    public ResponseEntity<AccountOut> login(
+        @RequestBody(required = true) LoginIn in);
 
     @PutMapping("/accounts/{id}")
     public ResponseEntity<AccountOut> update(
---
+++ src/main/java/humberto/loja/account/LoginIn.java
@@ -0,0 +1,11 @@
+package humberto.loja.account;
+
+import lombok.Builder;
+import lombok.experimental.Accessors;
+
+@Builder @Accessors(fluent = true, chain = true)
+public record LoginIn(
+    String email,
+    String password
+) {
+}
